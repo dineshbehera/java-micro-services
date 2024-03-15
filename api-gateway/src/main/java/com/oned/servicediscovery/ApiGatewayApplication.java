3605b9635b7a2acedc7d22f505b7f3e3e80c2c6e
Dinesh BEHERA <dinesh.plb@gmail.com>
2024-03-16 00:30:57 +0530

    Update ApiGatewayApplication.java

--- api-gateway/src/main/java/com/oned/servicediscovery/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/oned/servicediscovery/ApiGatewayApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiGatewayApplication {
 	public static void main(String[] args) {
-		System.out.println("Request received !");
+		System.out.println("Request received !"+ args.toString());
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
