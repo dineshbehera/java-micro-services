d07d99630330c7c6ae6c60cdbd523f37911824a8
Dinesh BEHERA <dinesh.plb@gmail.com>
2024-03-16 00:23:27 +0530

    Update HomeController.java

--- product/src/main/java/com/oned/product/controller/HomeController.java
+++ product/src/main/java/com/oned/product/controller/HomeController.java
@@ -2,18 +2,24 @@ package com.oned.product.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class HomeController {
 	
     Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+    
+    @Value("${message: default message}")
+    private String msg;
 
-    @GetMapping("/")
-	public String home() {
-		LOGGER.info("request received GET home()");
-		return "Product service is running !";
+    @GetMapping({"", "/"})
+	public String home(HttpServletRequest request) {
+		LOGGER.info(request.getRequestURL() +" is called !");
+		return request.getRequestURL() +" is called !"+ " Message : "+ msg;
 	}
 	
 
